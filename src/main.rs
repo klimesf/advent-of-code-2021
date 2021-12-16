c299d5ff6b763a1626faee5d6eebb1a24d2d71dc
Filip Klimeš <filip@filipklimes.cz>
2021-12-16 08:03:09 +0100

    day 16

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate itertools;
 extern crate priority_queue;
 
-use crate::day15::day15;
+use crate::day16::day16;
 
 mod io;
-mod day15;
+mod day16;
 
 fn main() {
-    day15();
+    day16();
 }
