673a8b8a3d67955224601b0d18ed83fbbb5905b9
Filip Klimeš <filip@filipklimes.cz>
2021-12-23 14:21:58 +0100

    day 24

---
+++ src/day24.rs
@@ -0,0 +1,8 @@
+use crate::io::read_lines;
+
+pub(crate) fn day24() {
+    let lines = read_lines("input/day24/input.txt").unwrap();
+    for line in lines.into_iter().map(|l| l.unwrap()) {
+        // TODO: have fun
+    }
+}
